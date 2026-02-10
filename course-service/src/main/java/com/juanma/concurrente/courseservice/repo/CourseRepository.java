be8e7d8b6b660e4419fdfbd54855f60b29598777
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2026-02-10 23:19:01 +0100

    feat(course): repositorio jpa para Course

--- course-service/src/main/java/com/juanma/concurrente/courseservice/repo/CourseRepository.java
+++ course-service/src/main/java/com/juanma/concurrente/courseservice/repo/CourseRepository.java
@@ -1,4 +1,11 @@
 package com.juanma.concurrente.courseservice.repo;
 
-public class CourseRepository {
+import com.juanma.concurrente.courseservice.domain.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    // repositorio jpa estandar
+    // hereda crud, paginacion y queries basicas
+    // no necesito metodos custom de momento
 }
