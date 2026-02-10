6079d9892dcbdc3a4b56ac5f777364b886256d85
Juan Manuel Torrado <jm.torrado.borrachero@gmail.com>
2026-02-10 23:17:20 +0100

    feat(db): h2 tcp server compartido y consola web

--- course-service/src/main/java/com/juanma/concurrente/courseservice/config/H2ServerConfig.java
+++ course-service/src/main/java/com/juanma/concurrente/courseservice/config/H2ServerConfig.java
@@ -1,4 +1,35 @@
 package com.juanma.concurrente.courseservice.config;
 
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
 public class H2ServerConfig {
+
+    // servidor h2 en modo tcp
+    // permite que varias apps (course, batch, etc) usen la misma bd en fichero
+
+    @Bean(name = "h2TcpServer", initMethod = "start", destroyMethod = "stop")
+    public Server h2TcpServer() throws SQLException {
+        return Server.createTcpServer(
+                "-tcp",
+                "-tcpAllowOthers",
+                "-tcpPort", "9092",
+                "-ifNotExists"
+        );
+    }
+
+    // consola web de h2
+    // util solo para inspeccionar datos en local
+    @Bean(name = "h2WebServer", initMethod = "start", destroyMethod = "stop")
+    public Server h2WebServer() throws SQLException {
+        return Server.createWebServer(
+                "-web",
+                "-webAllowOthers",
+                "-webPort", "8089"
+        );
+    }
 }
